88401652fb4cd74213321cced8e4a4c952bfd518
Anne Jochmaring <anne.jochmaring@de.clara.net>
2024-04-05 18:43:24 +0200

    not yet animated Larry

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,27 +1,38 @@
 extends CharacterBody2D
 
-@onready var animation_tree = $animation_tree
-@onready var animation_state = animation_tree.get("parameters/playback")
- 
-var input_movement = Vector2.ZERO
-var speed = 140
+@onready var _animation_player = $AnimationPlayer
+@export var speed = 140
 
-func _process(delta):
-	move()
 
-func move():
-	input_movement = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	if input_movement != Vector2.ZERO:
-		animation_tree.set("parameters/Idle/blend_position", input_movement)
-		animation_tree.set("parameters/Walk/blend_position", input_movement)
-		
-		animation_state.travel("Walk")
-		
-		velocity = input_movement * speed
-	
-	if input_movement == Vector2.ZERO:
-		animation_state.travel("Idle")
-		velocity = Vector2.ZERO
-	
+func _physics_process(_delta):
+	get_input()
 	move_and_slide()
+	
+func _process(_delta):
+	animate()
+
+
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
+
+func animate():
+	if Input.is_action_pressed("ui_right"):
+		_animation_player.play("WalkRight")
+	else:
+		_animation_player.stop()
+		
+	if Input.is_action_pressed("ui_left"):
+		_animation_player.play("WalkLeft")
+	else:
+		_animation_player.stop()
+		
+	if Input.is_action_pressed("ui_up"):
+		_animation_player.play("WalkUp")
+	else:
+		_animation_player.stop()
+		
+	if Input.is_action_pressed("ui_down"):
+		_animation_player.play("WalkDown")
+	else:
+		_animation_player.stop()

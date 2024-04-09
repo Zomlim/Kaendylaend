35aaab5cd3b81fa50dbf5fa893bf6388e586bc4e
Anne Jochmaring <anne.jochmaring@de.clara.net>
2024-04-09 13:57:13 +0200

    add idle animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -17,6 +17,10 @@ func get_input():
 	velocity = input_direction * speed
 
 func animate():
+	# set a default animation that does not override anything
+	if _animation_player.get_current_animation() == "":
+		_animation_player.play("IdleDown")
+	
 	# play each animation; to change, the animation gets overridden
 	# elif is required so an animation still plays when walking diagonal (ie two keys pressed simultaneously)
 	# order applies priorities from highest to lowest
@@ -29,8 +33,14 @@ func animate():
 	elif Input.is_action_pressed("ui_left"):
 		_animation_player.play("WalkLeft")
 		
-	
-
-	# if no key triggering any animation is pressed, stop the animation
+	# when player stops, idle animate
+	# applying the same principle and order as before
 	if not (Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down")):
-		_animation_player.stop()
+		if _animation_player.get_current_animation() == "WalkDown":
+			_animation_player.play("IdleDown")
+		elif _animation_player.get_current_animation() == "WalkUp":
+			_animation_player.play("IdleUp")
+		elif _animation_player.get_current_animation() == "WalkRight":
+			_animation_player.play("IdleRight")
+		elif _animation_player.get_current_animation() == "WalkLeft":
+			_animation_player.play("IdleLeft")
